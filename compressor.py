ff2139fd2d2af2fa67e397d78be10bd2ec42c487
dima <dakuljar@gmail.com>
2020-11-29 04:26:21 +0300

    Default Changelist

--- compressor.py
+++ compressor.py
@@ -5,10 +5,10 @@ import os
 print('*** Program Started ***')
 
 # image_font_path = '/home/conquistador/code/github/python-01-utilities/image/fonts/'
-image_path_input = '/Users/dima/Documents/dev/crawler-compressor/'
-image_path_output = '/Users/dima/Documents/dev/crawler-compressor/'
+image_path_input = '/Users/dima/Desktop/photo_compress_test/'
+image_path_output = '/Users/dima/Desktop/photo_compress_test/'
 # image_name_input = '05_compress_image_01_input.png'
-image_name_input = 'img-input.jpg'
+image_name_input = 'pro16.png'
 
 im = Image.open(image_path_input + image_name_input)
 print('Input file size   : ', im.size )
@@ -16,13 +16,13 @@ print('Input file name   : ', image_name_input )
 print('Input Image Size  : ', os.path.getsize (image_path_input  + image_name_input))
 
 # image_name_output = '05_compress_image_01_output.png'
-image_name_output = 'img_output.jpg'
+image_name_output = 'pro16_comp.png'
 
-im.save(image_path_output + image_name_output ,optimize=True,quality=50) 
+im.save(image_path_output + image_name_output, optimize=True, quality=50)
 
-print('Output file size  : ', im.size )
+print('Output file size  : ', im.size)
 print('Output file name  : ', image_name_output)
-print('Output Image Size : ', os.path.getsize (image_path_output + image_name_output))
+print('Output Image Size : ', os.path.getsize(image_path_output + image_name_output))
 
 print('*** Program Ended ***')
 x = input('Нажмите enter чтобы закрыть', )
